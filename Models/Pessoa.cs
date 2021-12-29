06b022ee338c75cc19020bb6e3f3e4e658bc27c8
brunolikma <brunosssss@gmail.com>
2021-12-29 14:32:44 -0300

    Curso concluido

---
+++ Models/Pessoa.cs
@@ -0,0 +1,26 @@
+namespace ExemploConstrutores.Models
+{
+    public class Pessoa
+    {
+        private readonly string nome = "Bruno";
+        private readonly string sobrenome;
+
+        public Pessoa()
+         {
+             nome = string.Empty;
+             sobrenome = string.Empty;
+         }
+        
+        public Pessoa(string nome, string sobrenome)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            System.Console.WriteLine("Contrutor Pessoa");
+        }
+
+        public void Apresentar()
+        {
+            System.Console.WriteLine($"Olá,meu nome é: {nome} {sobrenome}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+    class Program
+    {
+         public delegate void Operacao(int x, int y);
+         static void Main(string[] args)
+         {
+            Matematica m = new Matematica(20, 10);
+            m.Somar();
+
+        //     Operacao op = new Operacao(Calculadora.Somar);
+        //     op += Calculadora.Subtrair;
+        //     op.Invoke(10,10);
+            
+
+
+
+         //Data data = new Data();
+         //data.SetMes(20);
+
+         //data.Mes = 20;
+         //System.Console.WriteLine(data.Mes);
+
+         //data.ApresentarMes();
+            // Aluno p1 = new Aluno("Bruno", "Lima", "Matematica");
+            // p1.Apresentar();
+        }
+    }
+}
